c5d1df3be9e515ef2b9870b5587ebec848f0b11f
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-10 16:11:53 +0900

    Title: 사칙연산 연습, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251110/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b =  30;
+        int c = a+b;
+        int r1 = c/8; 
+        int r2 = c%8;
+
+        System.out.println(r1); 
+        System.out.println(r2); 
+    }
+}
