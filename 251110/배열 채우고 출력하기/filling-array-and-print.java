f847b4a9f1ad8220aff7a28db96c832e03416b6c
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-10 16:38:09 +0900

    Title: 배열 채우고 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 251110/배열 채우고 출력하기/filling-array-and-print.java
+++ 251110/배열 채우고 출력하기/filling-array-and-print.java
@@ -5,17 +5,19 @@ public class Main {
         
         Scanner sc = new Scanner(System.in);
 
-        // 문자 10개니까 문자  타입인 char로 선언
-        char ch[] = new char[10];
+        // 문자 10개를 저장할 수 있는 배열 생성
+        char[] arr = new char[10];
 
-        // 입력 받은 문자 저장 0~9까지 순호 
-        for(int i=0; i<ch.length; i++){
-            ch[i] = sc.next().charAt(0);
+        
+        // 배열을 순회한다. -> 반복문 사용 -> 사용 이유? -> 각 요소에 순서대로 접근해 특정 연산 수행 및 조건추가
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.next().charAt(0); // arr[0] = L, arr[1] = E, ...
+           // System.out.print(arr[i]+" "); // L E B R O S C O D E
         }
 
-        // 역순으로 출력
-        for(int i=ch.length-1; i>=0; i--){
-            System.out.print(ch[i]);
+        // 역순 출력 -는 해당 배열 또는 문자열 맨 뒤에서 부터 출력
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.print(arr[i]);
         }
     }
 }
