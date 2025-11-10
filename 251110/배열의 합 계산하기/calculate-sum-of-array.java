3aa8cc9ec1716ce7da08869d02ff4600eea038f9
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-10 14:42:50 +0900

    Title: 배열의 합 계산하기, Time: 84ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251110/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3,1,4,5,9};
+        int sum = arr[1]+arr[3]+arr[4];
+
+        System.out.println(sum);
+    }
+}
