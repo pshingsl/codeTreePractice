22d4a04f9631c1d07624ec3fd4cbd196ff6620d1
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-10 16:11:11 +0900

    Title: 변수 값 복사하기 3, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251110/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        int c = 3;
+      
+        a = c; // 3
+        a = a + c; // 3+3
+        b = b-c; // 5 - 3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
