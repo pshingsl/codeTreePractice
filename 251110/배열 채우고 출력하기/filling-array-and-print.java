f7d602474a5d16be241f7494c2db9ac6f2a053d7
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-10 16:29:53 +0900

    Title: 배열 채우고 출력하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 251110/배열 채우고 출력하기/filling-array-and-print.java
+++ 251110/배열 채우고 출력하기/filling-array-and-print.java
@@ -8,12 +8,13 @@ public class Main {
         // 문자 10개니까 문자  타입인 char로 선언
         char ch[] = new char[10];
 
-        // 입력 받은 문자들을 입력 받은 순서의 역순으로 출력
+        // 입력 받은 문자 저장 0~9까지 순호 
         for(int i=0; i<ch.length; i++){
             ch[i] = sc.next().charAt(0);
         }
 
-          for(int i=ch.length-1; i>=0; i--){
+        // 역순으로 출력
+        for(int i=ch.length-1; i>=0; i--){
             System.out.print(ch[i]);
         }
     }
