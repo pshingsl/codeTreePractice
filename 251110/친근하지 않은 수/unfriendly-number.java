c531a19ad87a320b1be73c66af3cef5b48be7f39
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-10 18:31:23 +0900

    Title: 친근하지 않은 수, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251110/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        // 1 2 3 4 5 6 7 8 9 10 -> 2,3 또는 5로 나누어 떨어진다.
+        // 2일 경우 -> 2 4 6 8 10
+        // 3일 경우 -> 3 6 9
+        // 5일 경우 -> 5 10
+
+        // n=10이면 1~10까지 반복 
+        for(int i=1; i<=n; i++){
+            if(i%2==0 && i%3==0 && i%5==0){
+                continue;
+            }
+        }
+    }
+}
