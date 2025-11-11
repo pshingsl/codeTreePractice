842a63076e19001fcc3c194c6ead82bb7c4b1fb5
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-11 14:08:05 +0900

    Title: 특정 수 맞히기, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251111/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+        int n = sc.nextInt();
+        if(n<25){
+            System.out.println("Higher");
+        }else if(n == 25){
+            System.out.println("Good");
+            break;
+        }else{
+             System.out.println("Lower");
+        }
+        }
+
+    }
+}
