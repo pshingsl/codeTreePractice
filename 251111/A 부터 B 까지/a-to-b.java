62864f45f852492c329c092469cb6740fae5d46f
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-11 14:15:51 +0900

    Title: A 부터 B 까지, Time: 156ms, Memory: 14MB, Status: Passed - Codetree

--- 251111/A 부터 B 까지/a-to-b.java
+++ 251111/A 부터 B 까지/a-to-b.java
@@ -8,12 +8,15 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i=a; i<=b;){
-             System.out.print(i + " ");
-            if(i%2==0){
-                i+=3;
+        // 2<=13 2,3,4,5,6,7,8,9,10,11,12,13
+
+        // 조건대로 출력하려면 else 끝나고 출력하면 안됨 -> 조건문을 검사 다하고 그때 부터 출력함
+        while(a<=b){
+              System.out.print(a+" ");
+            if(a%2==0){
+                a+=3;
             }else{
-                i*=2;
+                a*=2;
             }
         }
     }
