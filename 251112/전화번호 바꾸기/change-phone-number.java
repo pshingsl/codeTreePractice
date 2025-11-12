13d38a150c66d764fc856972c36990e19dd9830a
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-12 16:55:53 +0900

    Title: 전화번호 바꾸기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251112/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+
+       String s = sc.next();
+
+       String[] arr = s.split("-");
+
+       System.out.print(arr[0]+"-"+arr[2]+"-"+arr[1]);
+    }
+}
