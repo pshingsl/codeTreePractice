9528bfb453d1ae8b2bbe76b742358931fcf6875b
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-12 16:05:05 +0900

    Title: 입력받은 값과 합 출력, Time: 112ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251112/입력받은 값과 합 출력/output-of-value-and-sum.java
+++ 251112/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -8,6 +8,6 @@ public class Main {
        int a = sc.nextInt();
        int b = sc.nextInt();
 
-       System.out.print(a + " " + b + (a+b));
+       System.out.print(a + " " + b + " = "+ (a+b));
     }
 }
