ca9be0f442e4b09ee688198dfd42ad8651315b64
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-12 15:14:21 +0900

    Title: 변수 값 교체하기 6, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251112/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       
+       int a=2;
+       int b=5;
+
+       int tmp; 
+       tmp=5;
+       b=a;
+       a=tmp;
+
+       System.out.print(a+"\n"+b);
+    }
+}
