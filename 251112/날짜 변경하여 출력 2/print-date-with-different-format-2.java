4af9b3a1b020bc9f4011f3b80e92598dbb334919
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-12 16:43:23 +0900

    Title: 날짜 변경하여 출력 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251112/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] arr = s.split("-"); // 5-26-1994 5 26 1994분리
+
+        System.out.print(arr[2]+"."+ arr[1] +"." + arr[0]);
+    }
+}
