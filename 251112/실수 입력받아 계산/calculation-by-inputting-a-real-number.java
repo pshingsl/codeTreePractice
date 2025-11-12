3eca6cfc43229528e41b1861808fbc5c7492938f
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-12 16:08:32 +0900

    Title: 실수 입력받아 계산, Time: 145ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251112/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+
+       double a = sc.nextDouble();
+       double b = sc.nextDouble();
+
+       double re = a+b; 
+       System.out.printf("%.2f", re);
+    }
+}
