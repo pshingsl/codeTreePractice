5e2def9a79087b3dae734fdc52932765ac856a80
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-13 17:20:27 +0900

    Title: 삼항연산자, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251113/삼항연산자/ternary-operator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        String re = score==100 ? "pass" : "failure";
+
+        System.out.print(re);
+    }
+}
