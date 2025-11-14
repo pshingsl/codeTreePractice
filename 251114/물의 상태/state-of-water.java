a09df99aea2cb1e87220dc1a6f83fa62c9d10bc0
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-14 16:30:26 +0900

    Title: 물의 상태, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251114/물의 상태/state-of-water.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a<0){
+            System.out.print("ice");
+        }else if(a>=100) {
+            System.out.print("vapor");
+        }else {
+            System.out.print("water");
+        }
+    }
+}
