cb1256c9d1acaaddd96e64467f714c97dab36553
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 17:04:35 +0900

    Title: 특정 조건 세 정수 비교, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251115/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 251115/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        // 변수 선언
+        int a, b, c;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        
+        if(a <= b && a <= c)
+            System.out.print("1 ");
+        else
+            System.out.print("0 ");
 
-        if(a == b || b==c){
-            System.out.print(1+ " ");
-        } else {
-            System.out.print(0+ " ");
-        }
-
-        if(a==c){
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-
+        if(a == b && b == c)
+            System.out.println("1");
+        else
+            System.out.println("0");
     }
 }
