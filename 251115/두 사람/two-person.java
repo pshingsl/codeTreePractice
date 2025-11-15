bd360a824fa9442ef28c707090561a02ea7f1939
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 18:19:07 +0900

    Title: 두 사람, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251115/두 사람/two-person.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int Aage = sc.nextInt();
+        String s = sc.next();
+
+        int Bage = sc.nextInt();
+        String m = sc.next();
+
+        // 두 사람 중 한 사람이라도 19세이상이면서 남자면 1출력
+        if(Aage >= 19&& s.equals("M") || Bage >= 19 && m.equals("M")){
+            System.out.print(1);
+        }else{
+            s.equals(0);
+        }
+
+        }
+}
