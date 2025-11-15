b3036bc7211c9c437e1bf818e4e4b3738d9c2601
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 16:43:00 +0900

    Title: 범위 안의 수, Time: 119ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251115/범위 안의 수/number-in-range.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+
+       int a = sc.nextInt();
+
+       if(10>=a && a<=20){
+        System.out.print("yes");
+       }else{
+        System.out.print("no");
+       }
+    }
+}
