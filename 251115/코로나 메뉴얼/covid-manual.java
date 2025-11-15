0345e05ca1a867a0876cada9a176dc8b280f564a
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 17:34:13 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251115/코로나 메뉴얼/covid-manual.java
+++ 251115/코로나 메뉴얼/covid-manual.java
@@ -8,11 +8,11 @@ public class Main {
         char c = sc.next();
         double d = sc.nextDouble();
 
-        System.out.print(c);'
+        
         // 감기 증상이 있으면 Y 없으면 N
         // d>=37 A d>=b d==36.5 
 
-
+        System.out.print(c);'
 
     }
 }
