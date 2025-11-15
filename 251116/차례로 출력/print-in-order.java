2a66adc18cb0c19271d4e7dcce55673749d5282b
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-16 03:42:29 +0900

    Title: 차례로 출력, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

--- 251116/차례로 출력/print-in-order.java
+++ 251116/차례로 출력/print-in-order.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         
-        for(int i =5; a<18; i++){
+        for(int i = 5; i < 18; i++){
             System.out.print(i+" ");
         }
     }
