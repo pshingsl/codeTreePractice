203ea54c33df702e4abef4e3b7a6b5a513f2069f
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 17:46:11 +0900

    Title: 굉장한 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251115/굉장한 수/amazing-number.java
+++ 251115/굉장한 수/amazing-number.java
@@ -6,11 +6,10 @@ public class Main {
 
         int n = sc.nextInt();
 
-        if(n%3==0 && n%2==1){
+        if((n%3==0 && n%2==1) || (n%5==0 && n%2==0')){
             System.out.println("true");
         }else{
             System.out.println("false");
-
         }
      
     }
