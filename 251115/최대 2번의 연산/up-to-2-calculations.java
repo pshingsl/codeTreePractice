722ac9a49ec4721af3396ea3b9dedd8a211fa45f
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 15:01:58 +0900

    Title: 최대 2번의 연산, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251115/최대 2번의 연산/up-to-2-calculations.java
+++ 251115/최대 2번의 연산/up-to-2-calculations.java
@@ -9,7 +9,7 @@ public class Main {
 
         // a가 짝수라면 2로 나눠준다  10/2 = 5
         if(a%2==0) {
-            System.out.println(((a/2)/2)+1); 
+            System.out.println((a/2)-2); 
         }
 
         // a가 홀수면 1을 더한 뒤 2로 나눠 준다. 3+1/2
