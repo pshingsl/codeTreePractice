251eb20f5fbe861c25b67524ec4552e6ab18c480
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 17:15:48 +0900

    Title: 둘 중 하나의 배수, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251115/둘 중 하나의 배수/multiple-of-either.java
+++ 251115/둘 중 하나의 배수/multiple-of-either.java
@@ -7,7 +7,7 @@ public class Main {
 
         int a = sc.nextInt();
 
-        if(a%3==0 || a>%5==0){
+        if(a%3==0 || a%5==0){
             System.out.print(1);
         }else{
             System.out.print(0);
