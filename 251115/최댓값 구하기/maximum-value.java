c6a3bfd0c047e558e2507a8c6c0b3e2fc3623ba8
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-15 17:28:02 +0900

    Title: 최댓값 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251115/최댓값 구하기/maximum-value.java
+++ 251115/최댓값 구하기/maximum-value.java
@@ -10,23 +10,22 @@ public class Main {
         int c = sc.nextInt();
 
 
-        // a b c 다 비교해서 최댓갑 구하기
-        if(a>=b){
-            if(a>=c){
-                System.out.print(a);
-            }
-        }
+        // a b c 다 비교해서 최댓갑 구하기 a>b a>c b>a b>c c>a c>b
+        // 1 1 0  11
+            if(a>=b){
+                if(a>=c){
+                    System.out.print(a);
+                }else{
+                    System.out.print(c);
+                }
 
-        if(b>=a){
-            if(b>=c){
-                System.out.print(b);
+                else{
+                    if(b>=c){
+                    System.out.print(b);
+                    }else{
+                    System.out.print(c);
+                    }
+                }
             }
-        }
-
-        if(c>=a){
-            if(c>=b){
-                System.out.print(c);
-            }
-        }
         }
     }
