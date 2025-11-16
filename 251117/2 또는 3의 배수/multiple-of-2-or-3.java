c0f6d67302169e057d9bd93f02605ebf986a68b9
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-17 02:44:52 +0900

    Title: 2 또는 3의 배수, Time: 127ms, Memory: 11MB, Status: Passed - Codetree

--- 251117/2 또는 3의 배수/multiple-of-2-or-3.java
+++ 251117/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -9,9 +9,9 @@ public class Main {
 
         for(int i=1; i<=n; i++){
             if(i%2==0 || i%3==0){
-                System.out.print(1);
+                System.out.print(1 + " ");
             }else{
-                System.out.print(0);
+                System.out.print(0 + " ");
             }
         }
     }
