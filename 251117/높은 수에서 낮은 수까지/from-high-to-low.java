9ad56255a5fbdd911f40270e5fc4e3d521b6a351
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-17 02:22:31 +0900

    Title: 높은 수에서 낮은 수까지, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251117/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+
+       if(b>a){
+        for(int i = b; i>=a; i--){
+            System.out.print(i+ " ");
+        }
+       }else{
+        for(int i = a; i>=b; i--){
+            System.out.print(i+ " ");
+        }
+       }
+    }
+}
