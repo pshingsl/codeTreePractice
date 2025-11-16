3e654325032ab958eb2f32300b08a478ee10c154
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-17 02:09:57 +0900

    Title: N번 더하기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251117/N번 더하기/add-n-times.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+
+       int a =  sc.nextInt();
+       int n =  sc.nextInt();
+
+      // 5~6 5+6 11+6 17+6 23+6 29+6 35+6
+       for(int i = a+n; i <= 7*n; i+=n){
+         System.out.println(i);
+       }
+    }
+}
