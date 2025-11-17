99e5f394d4da226af2b00c859b5664647a283d31
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-18 02:37:54 +0900

    Title: 윤년 횟수 구하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 251118/윤년 횟수 구하기/number-of-leap-years.java
+++ 251118/윤년 횟수 구하기/number-of-leap-years.java
@@ -7,19 +7,19 @@ public class Main {
 
        int n = sc.nextInt();
        int cnt = 0;
-       int cnt1 = 0;
+       int cnt1= 0;
 
        // 윤년 총 횟수
        // 윤년은 4로 나누어 떨어짐 
        // 100으로 나누어 떨어지면서 400으로 떨어지지 않음 
+       // 다 4의 배수 
        for(int i=1; i<=n; i++){
         if(i%4==0){
-            if(i%100==0 && i%400!=0){
-              cnt1++;
-            }
-            cnt++;
+          cnt++;
+          if(i%100==0 && i%400!=0)
+          cnt--;
         }
        }
-       System.out.print((cnt-cnt1));
+       System.out.print(cnt);
     }
 }
