813927a2a3ee6cac9ec607d3496b40e9010c66f5
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-18 00:14:48 +0900

    Title: 100점 도달하기, Time: 3066ms, Memory: 26MB, Status: Time Limit Exceed - Codetree

---
+++ 251118/100점 도달하기/reach-100-point.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+
+       int n = sc.nextInt();
+
+       int i=n;
+
+       while(i<=100){
+        if(n>=90){
+            System.out.print("A" + " ");
+            i++;
+        } else if(n>=80) {
+            System.out.print("B" + " ");
+            i++;
+        } else if(n>=70) {
+            System.out.print("C" + " ");
+             i++;
+        } else if(n>=60) {
+            System.out.print("D" + " ");
+        } else {
+            System.out.print("E" + " ");
+
+        }
+       }
+    }
+}
