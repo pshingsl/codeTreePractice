d3b3de9852a3663e238d808500abe4536fcaf55f
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-20 00:33:09 +0900

    Title: 친근하지 않은 수, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251120/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        // 1 2 3 4 5 6 7 8 9 10
+        for(int i = 1; i <= n; i++){
+            if(i%2!=0 && i%3!=0 && i%5!=0){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
