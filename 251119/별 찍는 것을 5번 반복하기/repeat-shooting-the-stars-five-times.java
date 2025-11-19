ea028cf39e00ec36c05921a5ac48d975190e2582
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-19 20:58:38 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 83ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251119/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void printStar(){
+        for(int i=0; i<5; i++){
+            System.out.println("**********");
+        }
+    }
+
+    public static void main(String[] args) {
+            printStar();
+    }
+}
