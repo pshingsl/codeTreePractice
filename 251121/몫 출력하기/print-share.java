4e2e064fdf13839521aa98706a32523e66503616
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-21 01:51:56 +0900

    Title: 몫 출력하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251121/몫 출력하기/print-share.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+
+        while(true) {
+            int a = sc.nextInt();
+
+            if(a%2==0){
+                a/=2;
+                a++;
+                System.out.println(a);
+                break;
+            }
+        } 
+    }
+}
