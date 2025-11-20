d410d7feee3ef35a7634e884ce290a9ff8b7b1bc
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-21 01:19:45 +0900

    Title: 1까지 나누기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251121/1까지 나누기/divide-by-1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;   
+
+ublic class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int i;
+        for(int i=1; i<=n; i++){
+            n/=i;
+            if(n<=1)
+            break;
+        }
+        System.out.print(i);
+    }
+}
