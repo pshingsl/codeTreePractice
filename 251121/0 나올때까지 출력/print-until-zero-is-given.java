d6db421db746be005f1c0ee25e219d4c016c9c5c
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-21 01:32:04 +0900

    Title: 0 나올때까지 출력, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 251121/0 나올때까지 출력/print-until-zero-is-given.java
+++ 251121/0 나올때까지 출력/print-until-zero-is-given.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i=1;
+        int cnt=0;
 
         while(true){
             int a = sc.nextInt();
             if(a==0){
-                System.out.println(i);
                 break;
             }
+            System.out.println(a);
+            i++;
         } 
     }
 }
