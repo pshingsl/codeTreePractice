43f3ae5d6d0b7b8a18dc3787dc1de9f0735de508
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-21 18:59:57 +0900

    Title: 공약수의 유무, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251121/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 251121/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
+    // 1920과 2880의 최대 공약수 (GCD) 값
+    private static final int GCD_VAL = 960; 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int a = sc.nextInt();
         int b = sc.nextInt();
         boolean satisfied = false;
 
-        for(int i=a; i<=b; i++){
-            if(i%920==0 && i%880==0){
-                satisfied = true;
+        for (int i = a; i <= b; i++) {
+            // ✅ 수정 1: i가 960의 약수(즉, 공약수)인지 확인하도록 조건을 변경합니다.
+            if (GCD_VAL % i == 0) { 
+                // ✅ 수정 2: 공약수를 찾았으므로 satisfied를 true로 변경합니다.
+                satisfied = true; 
+                // 루프 내에서 1을 출력할 필요는 없습니다. (루프 밖의 satisfied를 이용할 것이므로)
+                break; 
             }
         }
-       
-       if(satisfied==true){
-        System.out.print(1);
-       }
+        
+        // 루프 내에서 satisfied가 true로 변경되었다면 1을 출력합니다.
+        if (satisfied) { 
+            System.out.println(1);
+        }
+        // 그렇지 않다면 0을 출력합니다.
+        else { 
+            System.out.println(0); 
+        }
+        sc.close();
     }
 }
