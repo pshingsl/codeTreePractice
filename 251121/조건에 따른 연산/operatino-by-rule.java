885828608330fc294cceee5d07437d286ea69a4a
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-21 18:33:02 +0900

    Title: 조건에 따른 연산, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 251121/조건에 따른 연산/operatino-by-rule.java
+++ 251121/조건에 따른 연산/operatino-by-rule.java
@@ -7,13 +7,17 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
 
-        while(n<1000){
+        while(true){
             if(n%2==0){
                 n = n*3+1;
             }else{
                 n = n*2+2;
             }
             cnt++;
+
+            if(n>1000){
+                break;
+            }
         }
         System.out.print(cnt);
     }
