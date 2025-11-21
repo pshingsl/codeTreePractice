8099a37d5bb29a587fab6b865d2fd731ff58b959
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-21 19:06:08 +0900

    Title: ab 사이에 없는 c, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251121/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       int c = sc.nextInt();
+       boolean satis = true;
+
+       for(int i =a; i<=b; i++){
+        if(i%c==0){
+            satis = false;
+            break;
+        }
+       }
+
+       if(satis){
+        System.out.print("YES");
+       }else{
+        System.out.print("NO");
+       }
+    }
+}
