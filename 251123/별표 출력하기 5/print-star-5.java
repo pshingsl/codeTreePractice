a1c4d6150c261735098cbe16737511bd9225630d
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-23 02:27:32 +0900

    Title: 별표 출력하기 5, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251123/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i<n; i++){
+            for(int j = j< ; j++){
+                System.out.print("*")
+            }
+        }
+    }
+}
