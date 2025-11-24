f9b47c371c76b68eb489e5df9bc61255777fcdb5
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-25 01:29:40 +0900

    Title: 별 그리기, Time: 169ms, Memory: 11MB, Status: Passed - Codetree

--- 251125/별 그리기/star-drawing.java
+++ 251125/별 그리기/star-drawing.java
@@ -4,30 +4,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 변수 선언 및 입력
+
         int n = sc.nextInt();
 
-        // 모양에 맞게 윗쪽 별을 출력합니다.
-        for(int i = 0; i<n; i++){
-            for(int j = -i+1; j>=0; j--){
-                System.out.print(" ");
-            }
 
-            for(int j = 0; j< 2*i+1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        for(int i=0; i<n; i++){
+                for(int j =0; j<n-(i+1); j++){
+                    System.out.print(" ");
+                }
+                for(int j =0; j<(2*i)+1; j++){
+                    System.out.print("*");
+
+                }
+        System.out.println();
+
         }
 
-         for(int i = 1; i<n; i++){
-            for(int j = 0; j<=i; j++){
-                System.out.print(" ");
-            }
+         for(int i=n-2; i>=0; i--){
+                for(int j =0; j<n-(i+1); j++){
+                    System.out.print(" ");
+                }
+                for(int j =0; j<(2*i)+1; j++){
+                    System.out.print("*");
 
-            for (int j=-2*i+3; j>0; j--){
-                System.out.print("*");
-            }
-            System.out.println();
+                }
+        System.out.println();
+
         }
+
     }
 }
