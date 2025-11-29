97dc39eebc238b8fa5e171d4dbde2635d026a242
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-11-29 20:47:12 +0900

    Title: 코로나 메뉴얼, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251129/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        char ch1 = sc.next().charAt(0);
+        int a = sc.nextInt();
+
+        char ch2 = sc.next().charAt(0);
+        int a2 = sc.nextInt();
+
+        char ch3 = sc.next().charAt(0);
+        int a3 = sc.nextInt();
+
+        if(ch1 == 'Y' && a>=37){
+
+        }else{
+            System.out.print("D");
+        }
+    }
+}
