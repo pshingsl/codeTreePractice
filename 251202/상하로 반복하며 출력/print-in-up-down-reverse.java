dcc28af3b0caaf134ed5bb632477db296491cedc
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-02 16:10:10 +0900

    Title: 상하로 반복하며 출력, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251202/상하로 반복하며 출력/print-in-up-down-reverse.java
+++ 251202/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -8,7 +8,7 @@ public class Main {
 
         for(int i = 0; i<n; i++){
             for(int j = 0; j<n; j++){
-                if(i%2==0)
+                if(j%2==0)
                     System.out.print(i+1);
                 else
                     System.out.print(n-1);
