8fed839ee3422622e0504c016c8f7ec825bdeb8e
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-02 16:09:24 +0900

    Title: 상하로 반복하며 출력, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251202/상하로 반복하며 출력/print-in-up-down-reverse.java
+++ 251202/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
