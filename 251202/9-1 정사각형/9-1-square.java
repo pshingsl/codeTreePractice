787eadfe13f24deaf83de653e53f3b22c6fbb3f9
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-02 15:26:46 +0900

    Title: 9-1 정사각형, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251202/9-1 정사각형/9-1-square.java
+++ 251202/9-1 정사각형/9-1-square.java
@@ -6,9 +6,16 @@ public class Main {
 
         int n = sc.nextInt();
         int cnt = 9;
-        for(int i = n; i>=1 ; i--){
-            System.out.pritn(cnt);
-            cnt--;
+        for(int i = n; i >= 1; i--){
+            for(int j = n; j >= 1; j--){
+                System.out.print(cnt);
+                cnt--;
+
+                if(cnt<1){
+                    cnt = 9;
+                }
+            }
+            System.out.println();
         }
     }
 }
