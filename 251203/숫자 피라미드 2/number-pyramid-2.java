5da7ae36be6744e555b748770f0e2c1bcbd469af
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-03 18:15:10 +0900

    Title: 숫자 피라미드 2, Time: 189ms, Memory: 11MB, Status: Passed - Codetree

--- 251203/숫자 피라미드 2/number-pyramid-2.java
+++ 251203/숫자 피라미드 2/number-pyramid-2.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int cnt = 0;
+        int cnt = 1;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j <= i; j++){
