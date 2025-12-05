13dd912525d0dccc4d9ba67e7475321c0feb1b1b
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-06 01:32:38 +0900

    Title: 점수대 파악하기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251206/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[100];
+        int[] countArr = new int[11];
+
+        for(int i = 0; i<100; i++){
+            arr[i] = sc.nextInt();
+
+            if(arr[i] == 0){
+                break;
+            }
+
+            countArr[arr[i] / 10]++;
+        }
+
+        for(int i=10; i>=1; i--){
+            System.out.println(i+ "0- "+countArr[i]);
+        }
+    }
+}
