e54420be7cc89b7d8b589864ec5a0c76393d8f51
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-06 01:22:05 +0900

    Title: 십의 자리 숫자, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251206/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int[] counts = new int[10];
+
+        for(int i = 0; i < 100; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0){
+                break;
+            }
+            counts[arr[i]/10]++;
+        }
+    }
+}
