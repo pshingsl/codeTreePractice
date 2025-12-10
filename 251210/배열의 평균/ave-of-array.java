9444709ca4ceb1c920bccc0b77962f656a1e2b9e
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-10 17:33:18 +0900

    Title: 배열의 평균, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251210/배열의 평균/ave-of-array.java
+++ 251210/배열의 평균/ave-of-array.java
@@ -5,44 +5,35 @@ public class Main {
        Scanner sc = new Scanner(System.in);
 
        int arr[][] = new int[2][4];
+       int sum = 0;
 
-        int sum1 =0;
-        int sum2 =0;
-        int sum3 =0;
-        int sum4 =0;
-        int sum5 =0;
-        int sum6 =0;
-        int total =0;
-       for(int i=0; i<2; i++){
-            for(int j=0; j<4; j++){
-                arr[i][j]=sc.nextInt();
-                total += arr[i][j];
-
-                if(i==0){
-                    sum1+=arr[i][j];
-                }
-                else if(i==1){
-                    sum2+=arr[i][j];
-                }
-                
-                if(j==0){
-                    sum3+=arr[i][j];  
-                }
-                 else if(j==1){
-                    sum4+=arr[i][j];  
-                }
-                else if(j==2){
-                    sum5+=arr[i][j];  
-                }
-                else if(j==3){
-                    sum6+=arr[i][j];  
-                }
-            } 
+       for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            arr[i][j] = sc.nextInt();
+        }
        }
-            
-       System.out.printf("%.1f %.1f \n",(double)sum1/4,(double)sum2/4 );
-       System.out.printf("%.1f %.1f %.1f %.1f \n",(double)sum3/2,(double)sum4/2,(double)sum5/2,(double)sum6/2);
-       System.out.printf("%.1f",(double)total/8);
 
+       for(int i = 0; i < 2; i++){
+        int sum = 0;
+        for(int j = 0; j < 4; j++){
+            sum += arr[i][j];
+            System.out.printf("%.1f ", (double)sum/4);
+        }
+       }
+
+        for(int i = 0; i < 4; i++){
+        int sum = 0;
+        for(int j = 0; j < 2; j++){
+            sum += arr[i][j];
+            System.out.printf("%.1f ", (double)sum/2);
+        }
+       }
+        
+       for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 2; j++){
+            sum += arr[i][j];
+            System.out.printf("%.1f ", (double)sum/8);
+        }
+       }
     }
 }
