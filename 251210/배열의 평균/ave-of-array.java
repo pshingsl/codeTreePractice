238ca8a53e97a0604714d0dcba8dd497ac921387
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-10 17:34:15 +0900

    Title: 배열의 평균, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 251210/배열의 평균/ave-of-array.java
+++ 251210/배열의 평균/ave-of-array.java
@@ -2,38 +2,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-       int arr[][] = new int[2][4];
-       int sum = 0;
-
-       for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 4; j++){
-            arr[i][j] = sc.nextInt();
-        }
-       }
-
-       for(int i = 0; i < 2; i++){
+        // 2차원 배열을 구현합니다.
+        int[][] arr2d = new int[2][4];
         int sum = 0;
-        for(int j = 0; j < 4; j++){
-            sum += arr[i][j];
-            System.out.printf("%.1f ", (double)sum/4);
-        }
-       }
-
-        for(int i = 0; i < 4; i++){
-        int sum = 0;
-        for(int j = 0; j < 2; j++){
-            sum += arr[i][j];
-            System.out.printf("%.1f ", (double)sum/2);
-        }
-       }
         
-       for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 2; j++){
-            sum += arr[i][j];
-            System.out.printf("%.1f ", (double)sum/8);
+        // 각 줄마다 정수를 입력받습니다.
+        for(int i = 0; i < 2; i++)
+            for(int j = 0; j < 4; j++)
+                arr2d[i][j] = sc.nextInt();
+        
+        // 가로 평균을 출력합니다.
+        for(int i = 0; i < 2; i++) {
+            sum = 0;
+            for(int j = 0; j < 4; j++)
+                sum += arr2d[i][j];
+                System.out.printf("%.1f ", (double)sum / 4);
         }
-       }
+        System.out.println();
+
+        // 세로 평균을 출력합니다.
+        for(int j = 0; j < 4; j++) {
+            sum = 0;
+            for(int i = 0; i < 2; i++)
+                sum += arr2d[i][j];
+            System.out.printf("%.1f ", (double)sum / 2);
+        }
+        System.out.println();
+
+        // 전체 평균을 출력합니다.
+        sum = 0;
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 4; j++)
+                sum += arr2d[i][j];
+        }
+        System.out.printf("%.1f\n", (double)sum / 8);
     }
 }
+
