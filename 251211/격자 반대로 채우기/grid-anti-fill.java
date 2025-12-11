06e281656b3e82e4f9e84a1bc8d429286fd3160a
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-11 16:26:33 +0900

    Title: 격자 반대로 채우기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251211/격자 반대로 채우기/grid-anti-fill.java
+++ 251211/격자 반대로 채우기/grid-anti-fill.java
@@ -4,17 +4,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] arr = new int[10][10];
         int n = sc.nextInt();
-        int cnt = 1;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                arr[i][j] = cnt*cnt;
-                cnt++''
-                System.out.print(arr[i][j] + " ");
+        int arr[][]= new int[n][n];
+
+        int num=1;
+        for(int j=n-1; j>=0; j--) {
+           
+           
+           
+           if(n%2==0){
+            if(j%2==1){   
+                for(int i=n-1; i>=0; i--){
+                    arr[i][j]=num++;
+                }
+            }
+        
+            else if(j%2==0){
+                for(int i=0; i<n; i++ ){
+                    arr[i][j]=num++;
+                }
+            }
+           }
+           else{
+            if(j%2==0){   
+                for(int i=n-1; i>=0; i--){
+                    arr[i][j]=num++;
+                }
+            }
+        
+            else if(j%2==1){
+                for(int i=0; i<n; i++ ){
+                    arr[i][j]=num++;
+                }
+            }
+           }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
+
+
     }
 }
