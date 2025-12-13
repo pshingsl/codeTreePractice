561a60a724125b8811ce684884a2da21f1f2f62e
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-14 01:26:14 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 251214/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.java
+++ 251214/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String[] arr = new String[]{"apple", "banana", "grape", "blueberry", "orange"};
-        int cnt = 0;
-        for(int i = 0; i < 5; i++){
-            char ch = sc.next().charAt(0);
-            if(arr[i].charAt(i) == ch)
-                cnt++;
+        char n = sc.next().charAt(0);
+        int sum=0;
+        String []arr = new String[]{"apple", "banana", "grape", "blueberry", "orange"};
+        for (int i=0;i<5;i++){
+            for (int j=2;j<4;j++){
+                if(arr[i].charAt(j)==n){
+                    sum++;
+                    System.out.println(arr[i]);
+                }
+            }
         }
-        System.out.print(cnt);
+        System.out.print(sum);
     }
 }
+
