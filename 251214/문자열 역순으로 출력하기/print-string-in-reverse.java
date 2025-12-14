3a9785158cd4a3098860cf2cd8e4055a189211a1
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-14 16:18:50 +0900

    Title: 문자열 역순으로 출력하기, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 251214/문자열 역순으로 출력하기/print-string-in-reverse.java
+++ 251214/문자열 역순으로 출력하기/print-string-in-reverse.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 4;
         String[] arr = new String[4];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < 4; i++){
             arr[i] = sc.next();
         }
 
-        for(int i = n-1; i >= 0; i--){
+        for(int i = 3; i >=0; i--){
             System.out.println(arr[i]);
         }
-
+       
     }
 }
