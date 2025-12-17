60d3637b9c32d96974a9a0051f2d23c06d23e339
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-17 15:25:28 +0900

    Title: a로 채워넣기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251217/a로 채워넣기/filling-with-a.java
+++ 251217/a로 채워넣기/filling-with-a.java
@@ -6,7 +6,7 @@ public class Main {
 
         String str = sc.next();
         
-        String str1 = str.substring(0, 3) + "a" + str.substring(3, str.length()-2)+"a"+str.substring(str.length()-1);
+        String str1 = str.substring(0, 1) + "a" + str.substring(2, str.length()-2)+"a"+str.substring(str.length()-1);
         System.out.print(str1);
     }
 }
