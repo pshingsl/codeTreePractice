7ebdbbdaaa30cae252c42e90cdda9613caf584e5
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-17 15:37:43 +0900

    Title: 첫 번째와 두 번째 교환, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

--- 251217/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
+++ 251217/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-       String str = sc.next();
+        // 문자열을 입력받습니다.
+        String str = sc.next();
 
-       char arr[] = str.toCharArray();
-       
-      char x =arr[0];
-      char y = arr[1];
-       for(int i = 0; i<arr.length; i++){
-            if(arr[i]==x){
-                arr[i]=y;
-            }
-            else if(arr[i]==y){
-                arr[i]=x;
-            }
+        // 문자열의 첫 번째 문자와 두 번째 문자를 저장합니다.
+        char a = str.charAt(0);
+        char b = str.charAt(1);
         
-            System.out.print(arr[i]);
-       }    
+        // 문자열을 순회하면서 첫 번째 문자와 두 번째 문자를 교환합니다.
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == a)
+                str = str.substring(0, i) + b + str.substring(i + 1);
+            else if(str.charAt(i) == b)
+                str = str.substring(0, i) + a + str.substring(i + 1);
+        }
 
-    
-
+        // 교환된 이후의 문자열을 출력합니다.
+        System.out.println(str);
     }
 }
