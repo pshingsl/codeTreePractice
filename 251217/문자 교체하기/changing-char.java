178d886d2c9124beb4d4cd6077080716551b3687
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-17 15:32:04 +0900

    Title: 문자 교체하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251217/문자 교체하기/changing-char.java
+++ 251217/문자 교체하기/changing-char.java
@@ -2,37 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // 입력 예시:
-        // str: tcKVBgGSsqJrWvcJOme
-        // str2: dsLPQysTnUVMeBXLR
-        
-        String str = sc.next();   
-        String str2 = sc.next();  
-        
-        String part1; // str의 첫 두 글자 ("tc")
-        String part2; // str2의 두 번째 글자부터 끝까지 ("sLPQysTnUVMeBXLR")
+       Scanner sc = new Scanner(System.in);
 
-        // 1. str의 첫 두 글자 추출 (인덱스 0부터 2 전까지)
-        // 만약 str이 2글자 미만일 경우 IndexOutOfBoundsException 방지 로직 필요
-        if (str.length() >= 2) {
-            part1 = str.substring(0, 2);
-        } else {
-            part1 = str; // 2글자 미만이면 전체 사용 (문제 조건에 따라 조정)
-        }
-        
-        // 2. str2의 두 번째 글자부터 끝까지 추출 (인덱스 1부터 끝까지)
-        // str2가 1글자 이하일 경우 IndexOutOfBoundsException 방지 로직 필요
-        if (str2.length() >= 2) {
-            part2 = str2.substring(1); // 인덱스 1부터 끝까지
-        } else {
-            part2 = ""; // 1글자 이하면 빈 문자열 (문제 조건에 따라 조정)
-        }
-        
-        // 3. 두 부분을 합쳐 출력
-        System.out.print(part1 + part2);
-        
-        sc.close();
+       String str = sc.next();
+       String str2 = sc.next();
+
+       str =  str.substring(0,2);
+       str2 = str2.substring(str2.length()-4);
+
+       System.out.print(str + str2);
     }
 }
