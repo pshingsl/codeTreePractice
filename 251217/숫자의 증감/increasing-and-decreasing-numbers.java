cfa5544a9048be0aa6f8debd89012586b762d632
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-17 16:25:36 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251217/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+
+       char C  = sc.next();
+       int N = sc.next();
+       
+       for(int i = 1; i<=n; i++){
+        if(C == 'A'){
+            System.out.print(i+" ");
+        }
+       }
+
+        for(int i = n; i>=n; i--){
+        if(C == 'D'){
+            System.out.print(i+" ");
+        }
+       }
+    }
+}
