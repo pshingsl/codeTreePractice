6e6fa8986985b358fc52c57ea37e75ba1101643d
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-19 00:31:20 +0900

    Title: 그 다음 알파벳, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251219/그 다음 알파벳/next-alphabet.java
+++ 251219/그 다음 알파벳/next-alphabet.java
@@ -3,26 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
         
-        // 1. 문자 입력
-        char inputChar = sc.next().charAt(0);
-        
-        char nextChar;
-        
-        // 2. 'z' 예외 처리
-        if (inputChar == 'z') {
-            nextChar = 'a';
-        } 
-        // 3. 일반적인 경우 (아스키 코드에 1을 더함)
-        else {
-            // (int)inputChar + 1 : 다음 문자의 아스키 코드 값
-            // (char)로 다시 형 변환하여 다음 문자를 얻습니다.
-            nextChar = (char)(inputChar + 1);
+        if(a == 'z'){
+            a = 'a';
+            System.out.print(a);
+        }else {
+            a = (int)a + 1;
+            System.out.print((char)(a+1);)
         }
-        
-        // 4. 결과 출력
-        System.out.println(nextChar);
-        
-        sc.close();
     }
 }
