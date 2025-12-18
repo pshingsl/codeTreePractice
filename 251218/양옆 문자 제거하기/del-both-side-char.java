da6972be3b9f428ad06ade422178831b93c0cbc4
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-18 18:14:20 +0900

    Title: 양옆 문자 제거하기, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251218/양옆 문자 제거하기/del-both-side-char.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s =sc.next();
+        int len = s.length();
+        System.out.print(s.substring(0,1)+s.substring(2,len-2));
+        System.out.print(s.substring(len-1));
+
+    }
+}
+
