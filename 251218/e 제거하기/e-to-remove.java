297f3f9641a2942be878ba7f6b10970fd6a68b44
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-18 18:22:04 +0900

    Title: e 제거하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251218/e 제거하기/e-to-remove.java
+++ 251218/e 제거하기/e-to-remove.java
@@ -7,26 +7,44 @@ public class Main {
         // 1. 문자열 입력
         String str = sc.next();
         
-        // 2. 제거할 문자 식별
-        char firstChar = str.charAt(0);
-        char lastChar = str.charAt(str.length() - 1);
+        // 2. 'e'가 가장 처음 등장하는 인덱스 찾기
+        // 문제 조건에 따라 'e'는 항상 존재하므로 -1이 반환될 걱정은 없습니다.
+        int firstEIndex = str.indexOf('e');
         
-        // 3. 새로운 문자열 생성 (StringBuilder 사용 권장)
-        StringBuilder result = new StringBuilder();
+        // 3. 해당 인덱스의 'e'를 제외하고 문자열 재구성
+        String result = "";
         
-        // 4. 문자열 순회 및 문자 제거 로직
-        for (int i = 0; i < str.length(); i++) {
-            char currentChar = str.charAt(i);
-            
-            // 현재 문자가 첫 문자 또는 마지막 문자의 값과 같으면 제거(추가 안 함)
-            if (currentChar != firstChar && currentChar != lastChar) {
-                result.append(currentChar);
-            }
+        // 3-1. 첫 번째 'e' 이전 부분 (인덱스 0부터 firstEIndex - 1까지)
+        // substring(startIndex, endIndex)에서 endIndex는 포함되지 않음
+        if (firstEIndex > 0) {
+            result += str.substring(0, firstEIndex);
         }
         
-        // 5. 결과 출력
-        System.out.println(result.toString());
+        // 3-2. 첫 번째 'e' 이후 부분 (인덱스 firstEIndex + 1부터 끝까지)
+        // substring(startIndex)는 startIndex부터 끝까지를 의미
+        if (firstEIndex < str.length() - 1) {
+            result += str.substring(firstEIndex + 1);
+        }
+        
+        // 4. 결과 출력
+        System.out.println(result);
         
         sc.close();
     }
 }
+
+// 참고: StringBuilder를 사용하는 더 간단한 방법
+/*
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int firstEIndex = str.indexOf('e');
+        
+        StringBuilder sb = new StringBuilder(str);
+        sb.deleteCharAt(firstEIndex);
+        
+        System.out.println(sb.toString());
+    }
+}
+*/
