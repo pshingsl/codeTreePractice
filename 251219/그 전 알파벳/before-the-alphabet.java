6dd852cb62705d2f4b72266261cc808b9eb2336b
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-19 00:34:30 +0900

    Title: 그 전 알파벳, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251219/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+        
+        if(a == 'a'){
+            a = 'z';
+            System.out.print(a);
+        }else {
+            System.out.print((char)(a-1));
+        }
+    }
+}
