626d0eb4259079a0c8e130f4a49f71179d2ecdc3
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-20 00:46:53 +0900

    Title: 소문자와 숫자, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 251220/소문자와 숫자/letter-and-number.java
+++ 251220/소문자와 숫자/letter-and-number.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
 
-        String str = sc.next();
+       String str = sc.next();
 
-        int len = str.length();
-
-        str = str.toLowerCase();
-
-        for(int i = 0; i < len; i++) {
-            if(Character.isLetter(str.charAt(i)) || Character.isDigit(str.charAt(i))) {
-                System.out.print(str.charAt(i));
-            }
-
+       for(int i = 0; i < str.length(); i++){
+        // 대문자 구분
+        if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z'){
+            System.out.print((char)(str.charAt(i) - 'A' + 'a'));
         }
+
+        // 소문자 출력
+        if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z'){
+            System.out.print(str.charAt(i));
+        }
+
+        // 숫자 출력
+         if(str.charAt(i) >= '0' && str.charAt(i) <= '9'){
+            System.out.print(str.charAt(i));
+        }
+       }
     }
 }
