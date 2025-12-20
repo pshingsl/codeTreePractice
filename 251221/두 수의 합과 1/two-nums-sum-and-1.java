9f81dc0f09c24a381d6b8d06f25786da6f827f9f
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-21 01:34:26 +0900

    Title: 두 수의 합과 1, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251221/두 수의 합과 1/two-nums-sum-and-1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int s = a+b;    
+        int cnt = 0;
+
+        String str = Integer.toString(s);
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i)=='1'{
+                cnt++;
+            })
+        }
+        System.out.print(cnt);
+    }
+}
