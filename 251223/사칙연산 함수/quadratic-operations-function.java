8c578900838f34a33cadf0def733527cfb0f484c
pshingsl <101494064+pshingsl@users.noreply.github.com>
2025-12-23 02:04:20 +0900

    Title: 사칙연산 함수, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251223/사칙연산 함수/quadratic-operations-function.java
+++ 251223/사칙연산 함수/quadratic-operations-function.java
@@ -1,28 +1,28 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+                Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         char sline=sc.next().charAt(0);
         int c=sc.nextInt();
- 
+        
         System.out.println(a+" "+sline+" "+c+" = "+strToNumber(a,c));
        
     }
     public static int strToNumber(int a, int c){
-        String[] sline={"*/+-"};
-               int number=1;
-
-        for(int i=0;i<=sline.length-1;i++){
-            if(sline[i].contains("*"))
-                number=a*c;
-            else if(sline[i].contains("/"))
-                number=a/c;
-            else if(sline[i].contains("+"))
-                number=a+c;
-            else if(sline[i].contains("-"))
-                number=a-c;
-        }
+      
+        List<String> splitted=new ArrayList<>();
+        String sline="";
+        int number=0;
+        if(sline.contains("*"))
+            number=a*c;
+        else if(sline.contains("/"))
+            number=a/c;
+        else if(sline.contains("+"))
+            number=a+c;
+        else if(sline.contains("-"))
+            number=a-c;
+        
         return number;
     }
   
