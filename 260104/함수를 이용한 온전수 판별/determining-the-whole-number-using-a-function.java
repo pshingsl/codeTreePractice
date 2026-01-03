5a88d273a4b1a059d5c6581dff1355c0a097da99
pshingsl <101494064+pshingsl@users.noreply.github.com>
2026-01-04 01:37:21 +0900

    Title: 함수를 이용한 온전수 판별, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260104/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+        for(int i = a; i<=b; i++){
+            if(!check(i)){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+    public static boolean check(int a) {
+        if(a%2==0)
+            return true;
+        if(a%10==5)
+            return true;
+        if(a%3==0 && a%9!=0)
+             return true;
+
+        return false;
+    }
+}
