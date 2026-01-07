f40c36c2ed744305617131433461b82fc8e68ca7
pshingsl <101494064+pshingsl@users.noreply.github.com>
2026-01-08 01:19:26 +0900

    Title: 짝수만 2로 나누기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 260108/짝수만 2로 나누기/divide-even-numbers-by-2.java
+++ 260108/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-       
-        modify(arr);
 
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i] + " ");
-        }
+public class Main {
+    public static final int MAX_N = 50;
+
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+
+    public static void modify(int[] arr) {
+        for(int i = 0; i < n; i++)
+            if(arr[i] % 2 == 0)
+                arr[i] /= 2;
     }
 
-    public static void modify(int[] arr2){
-        for(int i=0; i < arr2.length(); i++){
-            if(arr2[i] % 2 == 0)
-                arr2[i] /= 2;
-        }
+    public static void main(String[] args) {
+        // 변수 선언 및 입력:
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        modify(arr);
+
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
     }
 }
