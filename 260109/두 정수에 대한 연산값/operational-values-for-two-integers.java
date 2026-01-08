2cfd9df62d6f8d2ce1df584fa5fea0e6f5897e6e
pshingsl <101494064+pshingsl@users.noreply.github.com>
2026-01-09 01:03:23 +0900

    Title: 두 정수에 대한 연산값, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 260109/두 정수에 대한 연산값/operational-values-for-two-integers.java
+++ 260109/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -10,9 +10,9 @@ public class Main {
     }
 
     public static String sum(int a, int b){
-        if(a>b){
-            return (b*2) + " " + (a+25);
-        }else{
+        if(a>b)
+            return (a+25) + " " + (b*2);
+        else{
             return (a*2) + " " + (b+25);
         }
 
