3f0cb89a6de3e78ba8d533f9ea5345b1cd7e2fc1
pshingsl <101494064+pshingsl@users.noreply.github.com>
2026-01-10 02:16:53 +0900

    Title: 절댓값으로 변경, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260110/절댓값으로 변경/find-the-absolute-value.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] copy = arr.clone();
+        funcabs(copy);
+        for(int i = 0 ; i < n; i++){
+            System.out.print(copy[i] + " ");
+        }
+    }
+
+    public static void Arr(int arr[]){
+        for(int i=0; arr.length(); i++){
+            arr[i] = Math.abs(arr[i]);
+        }
+    }
+}
