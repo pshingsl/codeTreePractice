4ca76b31d120e619f9466b79e5ab3d57e7d1645b
pshingsl <101494064+pshingsl@users.noreply.github.com>
2026-01-31 22:57:06 +0900

    Title: 문자열 정렬, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

--- 260131/문자열 정렬/string-sort.java
+++ 260131/문자열 정렬/string-sort.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+        String str = sc.next();
        char[] chars = str.toCharArray();
         Arrays.sort(chars);
         System.out.println(chars);
