64bfbb3928e26309c32e54d0a5421a1c6185422b
pshingsl <101494064+pshingsl@users.noreply.github.com>
2026-01-31 22:56:21 +0900

    Title: 문자열 정렬, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260131/문자열 정렬/string-sort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+       char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        System.out.println(chars);
+    }
+}
